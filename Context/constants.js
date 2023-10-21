56b003964c3b780817095ab9c4d0cdded4778125
harry2855 <hemanshuraheja@gmail.com>
2023-10-21 12:00:00 +0530

    files for interacting with front-end

---
+++ Context/constants.js
@@ -0,0 +1,4 @@
+import nftMarketPlace from './NFTMarketplace.json'
+
+export const NFTMarketplaceAddress = '0x12AEB7477e058258D72a8cF3fEB011591e15e8D0'
+export const NFTMarketplaceABI = nftMarketPlace.abi
