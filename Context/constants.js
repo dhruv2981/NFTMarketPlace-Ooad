cb0c530612e95a8bef44caa5291ad694a5996393
harry2855 <hemanshuraheja@gmail.com>
2023-11-05 12:00:00 +0530

    update smart contract address

--- Context/constants.js
+++ Context/constants.js
@@ -1,4 +1,5 @@
 import nftMarketPlace from './NFTMarketplace.json'
 
-export const NFTMarketplaceAddress = '0x12AEB7477e058258D72a8cF3fEB011591e15e8D0'
+export const NFTMarketplaceAddress =
+  '0xB0Dd1668Da7C284EcFc3903786db4AF3E83Fe82C'
 export const NFTMarketplaceABI = nftMarketPlace.abi
